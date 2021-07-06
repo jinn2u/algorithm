1c3956264c33cdfd7658714603e97eff046d7499
jinn2u <bsybear623@gmail.com>
2021-07-06 11:15:59 +0900

    0706

--- algorithm/js/util/약수 구하기.js
+++ algorithm/js/util/약수 구하기.js
@@ -1,6 +1,7 @@
 function yaksu(num){
+  if (num<2) return [num]
   let arr = []
-  for(let i=1; i<(num**0.5)+1; i++){
+  for(let i=1; i<parseInt(num**0.5)+1; i++){
     if(num%i === 0){
       i === num/i 
         ? arr = [...arr, i]
@@ -9,4 +10,4 @@ function yaksu(num){
   }
   return arr
 }
-console.log(yaksu(9)) // [1, 9, 3]
+
