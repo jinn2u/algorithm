1ca259057264f250d7ff26b0d73d1aec93017beb
jinn2u <bsybear623@gmail.com>
2021-09-08 14:03:37 +0900

    0908

--- 백준/t1.js
+++ 백준/t1.js
@@ -1,24 +1,6 @@
 const fs = require("fs");
-let [N, money] = fs
+let [_, ...arr] = fs
   .readFileSync("./data")
   .toString()
   .split(" ")
   .map((el) => +el);
-
-let res = [];
-if (26 * N < money || N > money) {
-  console.log("!");
-  return;
-}
-for (let i = 0; i < N; i++) {
-  res.push("A");
-  money -= 1;
-}
-while (money > 0) {
-  const idx = res.findIndex((el) => el === "A");
-  const plus = Math.min(money, 25);
-  res[idx] = String.fromCharCode(plus + 65);
-  money -= plus;
-}
-res.sort();
-console.log(res.join(""));
