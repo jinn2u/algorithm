a1b08c740ab01c247be5526d13e1bfa05e410182
jinn2u <bsybear623@gmail.com>
2021-09-15 23:54:47 +0900

    0915

--- 백준/그리디/1/2437.js
+++ 백준/그리디/1/2437.js
@@ -2,21 +2,17 @@ const fs = require("fs");
 let [_, ...arr] = fs.readFileSync("./data", "utf8").trim().split("\n");
 arr = arr[0].split(" ").map((e) => Number(e));
 
-const solution = (arr) => {
+const solution = () => {
   arr = arr.sort((a, b) => a - b);
-  if (arr[0] !== 1) {
-    return 1;
-  }
-  let sum = 0;
-  for (let i in arr) {
-    i = Number(i);
-    sum += arr[i];
-    if (sum + 1 < arr[i + 1]) {
-      return sum + 1;
-    }
-  }
 
-  return sum + 1;
+  let target = 1;
+  for (let num of arr) {
+    if (target < num) {
+      return target;
+    }
+    target += num;
+  }
+  return target;
 };
 
-console.log(solution(arr));
+console.log(solution());
