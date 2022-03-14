779f30b8460c4f1619565a48a8c5aa40837cd7bb
jinn2u <bsybear623@gmail.com>
2022-03-14 09:11:10 +0900

    0314 알고리즘 풀이

---
+++ 프로그래머스/01/숫자 문자열과 영단어.js
@@ -0,0 +1,14 @@
+const solution = (s) => {
+  s = s
+    .replace(/zero/g, 0)
+    .replace(/one/g, 1)
+    .replace(/two/g, 2)
+    .replace(/seven/g, 7)
+    .replace(/three/g, 3)
+    .replace(/four/g, 4)
+    .replace(/five/g, 5)
+    .replace(/six/g, 6)
+    .replace(/eight/g, 8)
+    .replace(/nine/g, 9);
+  return +s;
+};
